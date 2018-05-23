0cb1f8173108ac4bf97f7da6bd43a7098aaf0062
Zubair Ahmed <zubair1024@gmail.com>
2018-05-23 15:59:23 +0400

    CFD

--- app.js
+++ app.js
@@ -23,6 +23,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
+
+let transporter = nodemailer.createTransport({
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false, // true for 465, false for other ports
+  auth: {
+      user: account.user, // generated ethereal user
+      pass: account.pass // generated ethereal password
+  }
+});
+
 // Add a route to the sitemap
 app.get('/sitemap', sitemap(app))
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const nodemailer = require('nodemailer');
 
 router.get('/', (req, res) => {
   res.render('index', {
@@ -17,6 +18,31 @@ router.get('/dev', (req, res) => {
 router.get('/robots.txt', function (req, res) {
   res.type('text/plain');
   res.send(`Sitemap: https://247staywarmco.uk/sitemap\nUser-agent: *\nDisallow: /`);
+});
+
+router.post('/contact', function (req, res) {
+  // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Fred Foo 👻" <foo@example.com>', // sender address
+        to: 'bar@example.com, baz@example.com', // list of receivers
+        subject: 'Hello ✔', // Subject line
+        text: 'Hello world?', // plain text body
+        html: '<b>Hello world?</b>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+
 });
 
 module.exports = router;
